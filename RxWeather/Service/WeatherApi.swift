b93fc1c8b4fcd4982030027894cd477f50e1a659
csehuman <paullee20204@snu.ac.kr>
2022-10-17 16:39:14 +0900

    Step 2 - Setup weather api

--- RxWeather/Service/WeatherApi.swift
+++ RxWeather/Service/WeatherApi.swift
@@ -29,4 +29,62 @@ import CoreLocation
 import NSObject_Rx
 
 
-
+class OpenWeatherMapApi: NSObject, WeatherApiType {
+    private let summaryRelay = BehaviorRelay<WeatherDataType?>(value: nil)
+    
+    private let forecastRelay = BehaviorRelay<[WeatherDataType]>(value: [])
+    
+    private let urlSession = URLSession.shared
+    
+    private func fetchSummary(location: CLLocation) -> Observable<WeatherDataType?> {
+        return composeUrlRequest(endpoint: summaryEndpoint, from: location)
+            .flatMap(urlSession.rx.data(request:))
+            .map { data -> WeatherSummary in
+                let decoder = JSONDecoder()
+                return try decoder.decode(WeatherSummary.self, from: data)
+            }
+            .map {
+                WeatherData(summary: $0)
+            }
+            .catchAndReturn(nil)
+        
+//        return urlSession.rx.data(request: request)
+//            .map { data -> WeatherSummary in
+//                let decoder = JSONDecoder()
+//                return try decoder.decode(WeatherSummary.self, from: data)
+//            }
+//            .map {
+//                WeatherData(summary: $0)
+//            }
+//            .catchAndReturn(nil)
+    }
+    
+    private func fetchForecast(location: CLLocation) -> Observable<[WeatherDataType]> {
+        return composeUrlRequest(endpoint: forecastEndpoint, from: location)
+            .flatMap(urlSession.rx.data(request:))
+            .map { data -> [WeatherData] in
+                let decoder = JSONDecoder()
+                let forecast = try decoder.decode(Forecast.self, from: data)
+                
+                return forecast.list.map(WeatherData.init)
+            }
+            .catchAndReturn([])
+        
+    }
+    
+    @discardableResult
+    func fetch(location: CLLocation) -> RxSwift.Observable<(WeatherDataType?, [WeatherDataType])> {
+        let summary = fetchSummary(location: location)
+        let forecast = fetchForecast(location: location)
+        
+        Observable.zip(summary, forecast)
+            .subscribe(onNext: { [weak self] result in
+                self?.summaryRelay.accept(result.0)
+                self?.forecastRelay.accept(result.1)
+            })
+            .disposed(by: rx.disposeBag)
+        
+        return Observable.combineLatest(summaryRelay.asObservable(), forecastRelay.asObservable())
+    }
+    
+}

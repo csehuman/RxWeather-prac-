3fc31861207b6771c51009a0c346719b43a5a3f2
csehuman <paullee20204@snu.ac.kr>
2022-10-17 16:41:50 +0900

    Step 3 - Create static location provider

--- RxWeather/Service/StaticLocationProvider.swift
+++ RxWeather/Service/StaticLocationProvider.swift
@@ -24,3 +24,16 @@
 import Foundation
 import CoreLocation
 import RxSwift
+
+
+struct StaticLocationProvider: LocationProviderType {
+    @discardableResult
+    func currentLocation() -> RxSwift.Observable<CLLocation> {
+        return Observable.just(CLLocation.gangnamStation)
+    }
+    
+    @discardableResult
+    func currentAddress() -> RxSwift.Observable<String> {
+        return Observable.just("강남역")
+    }
+}
